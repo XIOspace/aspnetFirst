721d5b82f9ea516cd3524506e73a7b3bf61b948a
xio <qq1954556630@gmail.com>
2022-08-28 15:23:56 +0800

    Initial commit

---
+++ aspnetFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace aspnetFirst.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
